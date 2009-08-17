95166394b1f5931443e986b53aef127bd720ff7b
Adrián Cereto Massagué <ssorgatem@esdebian.org>
2009-08-17 23:07:02 +0000

    ***
    git-svn-id: http://video4fuze.googlecode.com/svn/trunk@14 8f0b585c-89e2-11de-a519-b78fc8232508

--- translations/v4f_ca.ts
+++ translations/v4f_ca.ts
@@ -1,66 +1,112 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS>
+<TS version="2.0" language="ca_ES">
+<context>
+    <name>@default</name>
+    <message>
+        <location filename="GUI/MainWindow.py" line="24"/>
+        <source>Unknown error</source>
+        <translation>Error desconegut</translation>
+    </message>
+</context>
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="82"/>
+        <location filename="GUI/Ui_MainWindow.py" line="94"/>
         <source>video4fuze</source>
-        <translation type="unfinished"></translation>
+        <translation></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="83"/>
-        <source>Input</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="GUI/Ui_MainWindow.py" line="84"/>
-        <source>Output</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="GUI/Ui_MainWindow.py" line="85"/>
+        <location filename="GUI/Ui_MainWindow.py" line="97"/>
         <source>Remove files</source>
-        <translation type="unfinished"></translation>
+        <translation>Treure fitxers</translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="86"/>
+        <location filename="GUI/Ui_MainWindow.py" line="98"/>
         <source>Convert!</source>
-        <translation type="unfinished"></translation>
+        <translation>Convertir!</translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="91"/>
+        <location filename="GUI/Ui_MainWindow.py" line="103"/>
         <source>Add files...</source>
-        <translation type="unfinished"></translation>
+        <translation>Afegir fitxers...</translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="107"/>
+        <source>Select output folder</source>
+        <translation>Carpeta de sortida</translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="101"/>
+        <source>File</source>
+        <translation>Fitxer</translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="102"/>
+        <source>Help</source>
+        <translation>Ajuda</translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="104"/>
+        <source>About video4fuze</source>
+        <translation>Sobre video4fuze</translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="105"/>
+        <source>About Qt</source>
+        <translation>Sobre Qt</translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="106"/>
+        <source>Remove selected files</source>
+        <translation>Treu els fitxers seleccionats</translation>
     </message>
     <message>
         <location filename="GUI/Ui_MainWindow.py" line="95"/>
-        <source>Select output folder</source>
+        <source>Input files</source>
+        <translation>Fitxers originals</translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="96"/>
+        <source>Output folder</source>
+        <translation>Carpeta destí</translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="27"/>
+        <source>Error</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="28"/>
+        <source>An error has ocurred:</source>
+        <translation>Ha ocorregut un error:</translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="37"/>
+        <source>Done</source>
+        <translation>Fet</translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="98"/>
+        <source>Select files to add to the convert queue</source>
+        <translation type="unfinished">Selecciona fitxers a afegir a la cua de conversió</translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="126"/>
+        <source>Abut video4fuze</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="89"/>
-        <source>File</source>
+        <location filename="GUI/MainWindow.py" line="129"/>
+        <source>This applications uses mencoder and avi-mux GUI (under wine where necessary) in order to convert your video files to be seen in you sansa fuze.
+
+Thanks to ewelot from the sansa forums for finding the way to convert the videos, without his findings this app wouldn&apos;t exist.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="90"/>
-        <source>Help</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="GUI/Ui_MainWindow.py" line="92"/>
-        <source>About video4fuze</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="GUI/Ui_MainWindow.py" line="93"/>
-        <source>About Qt</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="GUI/Ui_MainWindow.py" line="94"/>
-        <source>Remove selected files</source>
-        <translation type="unfinished"></translation>
+        <location filename="GUI/MainWindow.py" line="158"/>
+        <source>Select output directory</source>
+        <translation>Escull carpeta de destí</translation>
     </message>
 </context>
 </TS>
--- translations/v4f_en.ts
+++ translations/v4f_en.ts
@@ -1,65 +1,110 @@
 <?xml version="1.0" encoding="utf-8"?>
 <!DOCTYPE TS><TS version="1.1">
 <context>
+    <name>@default</name>
+    <message>
+        <location filename="GUI/MainWindow.py" line="24"/>
+        <source>Unknown error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>MainWindow</name>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="82"/>
+        <location filename="GUI/Ui_MainWindow.py" line="94"/>
         <source>video4fuze</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="83"/>
-        <source>Input</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="GUI/Ui_MainWindow.py" line="84"/>
-        <source>Output</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="GUI/Ui_MainWindow.py" line="85"/>
+        <location filename="GUI/Ui_MainWindow.py" line="97"/>
         <source>Remove files</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="86"/>
+        <location filename="GUI/Ui_MainWindow.py" line="98"/>
         <source>Convert!</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="91"/>
+        <location filename="GUI/Ui_MainWindow.py" line="103"/>
         <source>Add files...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="95"/>
+        <location filename="GUI/Ui_MainWindow.py" line="107"/>
         <source>Select output folder</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="89"/>
+        <location filename="GUI/Ui_MainWindow.py" line="101"/>
         <source>File</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="90"/>
+        <location filename="GUI/Ui_MainWindow.py" line="102"/>
         <source>Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="92"/>
+        <location filename="GUI/Ui_MainWindow.py" line="104"/>
         <source>About video4fuze</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="93"/>
+        <location filename="GUI/Ui_MainWindow.py" line="105"/>
         <source>About Qt</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="GUI/Ui_MainWindow.py" line="94"/>
+        <location filename="GUI/Ui_MainWindow.py" line="106"/>
         <source>Remove selected files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="95"/>
+        <source>Input files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="96"/>
+        <source>Output folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="27"/>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="28"/>
+        <source>An error has ocurred:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="37"/>
+        <source>Done</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="98"/>
+        <source>Select files to add to the convert queue</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="126"/>
+        <source>Abut video4fuze</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="129"/>
+        <source>This applications uses mencoder and avi-mux GUI (under wine where necessary) in order to convert your video files to be seen in you sansa fuze.
+
+Thanks to ewelot from the sansa forums for finding the way to convert the videos, without his findings this app wouldn&apos;t exist.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/MainWindow.py" line="158"/>
+        <source>Select output directory</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
