d176aabd372696338b63bfc711700a2c5efb053a
Adrián Cereto Massagué <ssorgatem@esdebian.org>
2009-08-17 01:18:51 +0000

    ***
    git-svn-id: http://video4fuze.googlecode.com/svn/trunk@9 8f0b585c-89e2-11de-a519-b78fc8232508

---
+++ translations/v4f_en.ts
@@ -0,0 +1,66 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="82"/>
+        <source>video4fuze</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="83"/>
+        <source>Input</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="84"/>
+        <source>Output</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="85"/>
+        <source>Remove files</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="86"/>
+        <source>Convert!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="91"/>
+        <source>Add files...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="95"/>
+        <source>Select output folder</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="89"/>
+        <source>File</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="90"/>
+        <source>Help</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="92"/>
+        <source>About video4fuze</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="93"/>
+        <source>About Qt</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="GUI/Ui_MainWindow.py" line="94"/>
+        <source>Remove selected files</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
