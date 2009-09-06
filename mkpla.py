33873e5535b5c80430d269511a461a4bf70d565b
Adrián Cereto Massagué <ssorgatem@esdebian.org>
2009-09-06 22:53:57 +0000

    ***
    git-svn-id: http://video4fuze.googlecode.com/svn/trunk@33 8f0b585c-89e2-11de-a519-b78fc8232508

---
+++ mkpla.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#This module is inspired in Dunny's YAPL, and has been possible to write thanks to his help about the
+# .pla playlist format.
+
+#Internal memory is /mmc:0/
+#µSD is /mmc:1/
+
+#Line endings have to be DOS CR+LF
+
+import os
+
+#TODO: playlist creator/editor.
+
+#1 - get fuze's internal and µSD root directories
+
+#2 - Open (create) a playlist
+
+#3 - Put there songs, converting paths (maybe path normalisation?)
+
+#4 - Allow reordering and so, this should be a GUI thingcommalist
+
+#5 - Save the file.
