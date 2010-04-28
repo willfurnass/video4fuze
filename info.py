4c3c06e04417ab2a3891f3609225c0d3312955d6
Adrián Cereto Massagué <ssorgatem@esdebian.org>
2010-04-28 21:20:30 +0000

    Prepare for release
    git-svn-id: http://video4fuze.googlecode.com/svn/trunk@67 8f0b585c-89e2-11de-a519-b78fc8232508

--- info.py
+++ info.py
@@ -2,6 +2,6 @@
 AUTHORS = 'Adrián Cereto Massagué <ssorgatem@esdebian.org>'
 ORGNAME = 'ssorgatem productions'
 NAME = "video4fuze"
-VERSION = "0.5.0.9x+svn"
+VERSION = "0.5.1"
 URL = 'http://code.google.com/p/video4fuze'
-SHORT_DES = 'A tool to make videos compatible with the San Disk Sana Fuze media player'
+SHORT_DES = 'A tool to make videos compatible with the San Disk Sansa Fuze media player'
